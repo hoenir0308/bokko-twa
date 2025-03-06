41576503a43fb19ae3bf7c75e7437306c99e9e07
Ярослав Романов <84514763+romanovvINC@users.noreply.github.com>
2025-03-07 04:49:26 +0500

    Update Loader.tsx

--- bokko/src/components/ui/Loader/Loader.tsx
+++ bokko/src/components/ui/Loader/Loader.tsx
@@ -1,8 +1,8 @@
 import './Loader.css';
 import React from 'react';
 
-export const Loader = React.memo(() => (
-    <div className="lds-ring">
+export const Loader = React.memo(({isBlack, isSmall}: {isBlack?: boolean, isSmall?: boolean}) => (
+    <div className={`lds-ring ${isBlack ? `black` : ''} ${isSmall ? `small` : ''}`}>
         <div></div>
         <div></div>
         <div></div>
